4e5f920daaca20b56c576b38a391a39ba65639ff
Lucian AF <lucian_af@hotmail.com>
2021-11-30 22:53:23 -0300

    chore: adicionado configurações de base para os testes de integração;

---
+++ src/NerdStore.WebApp.Mvc/IntegrationTests/StartupTests.cs
@@ -0,0 +1,71 @@
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NerdStore.Catalogo.Application.AutoMapper;
+using NerdStore.Catalogo.Data.Context;
+using NerdStore.Pagamentos.Data;
+using NerdStore.Vendas.Data.Context;
+using NerdStore.WebApp.Mvc.Setup;
+
+namespace NerdStore.WebApp.Mvc
+{
+	public class StartupTests
+	{
+		public StartupTests(IConfiguration configuration)
+			=> Configuration = configuration;
+
+		public IConfiguration Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllersWithViews();
+
+			var connetionStrings = Configuration.GetConnectionString("DefaultConnection");
+
+			services.AddDbContext<CatalogoContext>(options => options.UseSqlServer(connetionStrings));
+			services.AddDbContext<VendasContext>(options => options.UseSqlServer(connetionStrings));
+			services.AddDbContext<PagamentoContext>(options => options.UseSqlServer(connetionStrings));
+
+			services.AddAutoMapper(
+				typeof(DomainToDtoMappingProfile),
+				typeof(DtoToDomainMappingProfile)
+				);
+
+			services.AddMediatR(typeof(StartupTests));
+
+			services.LoadAppSettings(Configuration);
+
+			services.RegisterServices();
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
+}
---
+++ tests/NerdStore.WebApp.Mvc.IntegrationTests/Configs/IntegrationTestsFixture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace NerdStore.WebApp.Mvc.IntegrationTests.Configs
+{
+	[CollectionDefinition(nameof(IntegrationTestsFixtureCollection))]
+	public class IntegrationTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupTests>> { }
+
+	public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
+	{
+		public readonly LojaAppFactory<TStartup> Factory;
+		public HttpClient Client;
+
+		public IntegrationTestsFixture()
+		{
+			var clientOptions = new WebApplicationFactoryClientOptions { };
+
+			Factory = new LojaAppFactory<TStartup>();
+			Client = Factory.CreateClient(clientOptions);
+		}
+
+		public void Dispose()
+		{
+			Factory.Dispose();
+			Client.Dispose();
+			GC.SuppressFinalize(this);
+		}
+	}
+}
